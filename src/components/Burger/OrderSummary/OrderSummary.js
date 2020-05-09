bd1577504134066a8f0fa2cc14022b9a845d892a
bwywb00 <bwywb00@gmail.com>
2020-05-09 21:44:16 +0900

    154. Imporving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map((igKey, i) => {
-            return (
-                <li key={igKey + i}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-                </li>);
-        })
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>{ingredientSummary}</ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Coutinue to Checkout?</p>
-            <Button
-                btnType="Danger"
-                clicked={props.purchaseCancelled}
-            >
-                CANCEL
+class orderSummary extends Component {
+
+    UNSAFE_componentWillUpdate() {
+        console.log('[OrderSummary will Update]');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map((igKey, i) => {
+                return (
+                    <li key={igKey + i}>
+                        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>);
+            })
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>{ingredientSummary}</ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Coutinue to Checkout?</p>
+                <Button
+                    btnType="Danger"
+                    clicked={this.props.purchaseCancelled}
+                >
+                    CANCEL
             </Button>
-            <Button
-                btnType="Success"
-                clicked={props.pruchaseContiuned}
-            >
-                COUTINUE
+                <Button
+                    btnType="Success"
+                    clicked={this.props.pruchaseContiuned}
+                >
+                    COUTINUE
             </Button>
-        </Aux>
-    )
-};
+            </Aux>
+        )
+    }
+}
 
 export default orderSummary;
