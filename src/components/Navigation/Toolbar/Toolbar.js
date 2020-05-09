6c0d264e82da0bb410903eb909a19e48e284167e
bwywb00 <bwywb00@gmail.com>
2020-05-09 20:32:22 +0900

    148. More about Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -10,10 +10,10 @@ const toolbar = (props) => (
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
-    </header>
+    </header >
 );
 
 export default toolbar;
