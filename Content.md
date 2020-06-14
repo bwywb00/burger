8bf70051166ceecce3a47eaac5a5b61bb8cd2fec
bwywb00 <bwywb00@gmail.com>
2020-06-14 18:36:33 +0900

    ~347. Sharing the Validation Method

--- Content.md
+++ Content.md
@@ -1,9 +1,4 @@
 344~350. **Improving Burger Builder**
-345. Fixing the Redirect to the Frontpage
+347. Sharing the Validation Method
 
-token이 존재하지 않을 때 Ingredient를 추가하고 Login을 하면 '/checkout'으로 Redirect해야 하는데
-App.js에서 token이 존재할 때 Auth Component에 대한 Route가 존재하지 않기 때문에 그냥 '/'로 보내버린다
-token이 존재할 때 Auth Component에 대한 Route를 추가하자
-
-SideDrawer에서 Link를 클릭해도 Drawer가 사라지지 않는 현상을 해결하자
-
+중복해서 사용되고 있는 checkValidity()를 utility.js에 넣고 불러서 사용하자
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from '../../../axios-orders';
 import { connect } from 'react-redux';
 
+import { checkValidity } from '../../../shared/utility';
+
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -118,34 +120,6 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token);
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -156,7 +130,7 @@ class ContactData extends Component {
         };
 
         updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
 
